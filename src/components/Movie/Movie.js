af0f537eaeebb2a3cd467de553963f5d12c99419
Batin Eryilmaz <batineryilmaz@icloud.com>
2020-03-24 18:18:32 +0300

    placeholder fix

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import './Movie.scss'
 import store from '../../../store'
-
+import placeholder from '../../images/Placeholder.png'
 const Movie = (props) => {
   const [isFav, setIsFav] = useState(false)
 
@@ -29,7 +29,7 @@ const Movie = (props) => {
     <div className=''>
       <div className='card movie-card'>
         <h4 className='card-title text-center'>{props.data.Title}</h4>
-        <img src={props.data.Poster !== 'N/A' ? props.data.Poster : '../../Images/Placeholder.png'} className='card-img align-middle' alt='...' />
+        <img src={props.data.Poster !== 'N/A' ? props.data.Poster : 'https://www.netclipart.com/pp/m/34-343827_transparent-background-question-mark-grey-png.png'} className='card-img align-middle' alt='...' />
         <div className='card-body'>
           <p className='card-text'><small className='text-muted'> Released: {props.data.Year}</small></p>
           <p className='card-text'><small className='text-muted'> IMDB Rating: {props.data.imdbRating}</small></p>
